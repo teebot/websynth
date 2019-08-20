c1ceeed6d4f313651de5aa5795152c92f1f35b78
teebot <thibaut@cozypixel.com>
2019-08-20 22:52:49 +0200

    Removed webpack config files

--- webpack.config.build.js
+++
@@ -1,9 +0,0 @@
-const webpack = require('webpack');
-const baseConfig = require('./webpack.config');
-
-module.exports = Object.assign(baseConfig, {
-  devtool: 'none',
-  optimization: {
-    minimize: true
-  }
-});
